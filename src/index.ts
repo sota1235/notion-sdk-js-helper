fd883fe1a63e48ed52868c98fcf9ff5a8bd57cc2
sota1235 <sota1235@gmail.com>
2022-11-27 22:36:46 +0900

    modify: update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,3 @@
-export * as BlockHelper from './blockObjects';
+export * as BlockObjects from './blockObjects';
+export * as RichTextObjets from './richTextObject';
+export * as CustomTypes from './customTypes';
