a5f44bd1581999452891f2bee77cc59a1c4e5ee0
sota1235 <sota1235@gmail.com>
2025-09-05 23:20:09 +0900

    fix: official sdk supported

--- src/typeValidation.ts
+++ src/typeValidation.ts
@@ -1,25 +1,5 @@
-import type {
-  BlockObjectResponse,
-  CreatePageResponse,
-  GetPageResponse,
-  ListBlockChildrenResponse,
-  PageObjectResponse,
-  UpdatePageResponse,
-} from "@notionhq/client/build/src/api-endpoints.js";
+import type { BlockObjectResponse } from "@notionhq/client/build/src/api-endpoints.js";
 
-// Check by the presence or absence of the url parameter
-export const isPageObjectResponse = (
-  page: CreatePageResponse | GetPageResponse | UpdatePageResponse,
-): page is PageObjectResponse => {
-  return (page as any).url !== undefined;
-};
-
-// BlockObjectResponse
-export const isBlockObjectResponse = (
-  result: ListBlockChildrenResponse["results"][0],
-): result is BlockObjectResponse => {
-  return (result as any).type !== undefined;
-};
 export type SomeBlockObjectResponse<T extends string> = Extract<
   BlockObjectResponse,
   { type: T }
